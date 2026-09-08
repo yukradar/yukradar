dabf1acfb07190135822dee35ea17ef44d56679d
yukradar <leventsahinlojistik@gmail.com>
2026-09-08 11:07:00 +0000

    guncelleme

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const SUPABASE_SERVICE_KEY = "sb_publishable_cqK2O5-DBEPzhqOmC4yFVg_WIQDBGys";
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
 // Gemini API Anahtarı
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "GERCEK_API_KEY_BURAYA";
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY || "GERCEK_AQ.Ab8RN6KyZniAjtZyN9Ja4ckRAOPv2NFvIDxFkBRW09-w_wG6JAAPI_KEY_BURAYA";
 
 app.post('/api/incoming-post', async (req, res) => {
   try {
